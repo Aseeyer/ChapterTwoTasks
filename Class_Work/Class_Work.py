3675bd0f012de1ede7afd3bc99be95b9f91b2928
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-07-16 11:44:41 +0100

    Initial Commit - Python assignments

---
+++ Class_Work/Class_Work.py
@@ -0,0 +1,20 @@
+""" welcome to your new role at our fintech company! as part of your onboarding, you're tasked with developing a user-friendly mortgage calculator console application for our loan officers. This application will streamline the process of calculating monthly mortgage payments for our clients
+here are the key features to consider:
+principal: the amount the client wishes to borrow.
+annual interest rate: The yearly interest rate offered on the mortgage. 
+Duration: The duration of the mortgage in years.
+The application should ask for these data and automatically calculate the monthly payment for the client 
+
+tip: to get the percentage monthly rate, you divide the annual interest rate by percentage and number of months in a year and to get the number of months from the duration you multiply it  by number of months in a year. """
+
+
+principal = float(input("Enter the principal amount: "))
+annual_interest_rate = float(input("Enter the annual interest rate: "))
+annual_duration = int(input("Enter the duration of the loan: "))
+
+monthly_rate = float((annual_interest_rate/100) / 12)
+monthly_duration = int(annual_duration * 12)
+
+monthly_payment = principal * (monthly_rate * (1 + monthly_rate) ** monthly_duration) / ((1 + monthly_rate) ** monthly_duration - 1)
+
+print("The Monthly payment on a principal on #", principal ,"is" , monthly_payment)
