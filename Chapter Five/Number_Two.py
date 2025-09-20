029b81bb5420dac0b439c18cfb426157d19788f7
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-09-20 20:27:31 +0100

    added 5.2

---
+++ Chapter Five/Number_Two.py
@@ -0,0 +1,9 @@
+"""
+def mystery(sequence):
+    return sequence == sorted(sequence)
+
+The function checks whether the sequence is already sorted in ascending order.
+It compares the original sequence with a sorted version of it.
+If they are the same, it returns True. Otherwise, it returns False.
+
+"""
