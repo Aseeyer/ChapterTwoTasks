00024fc3273fbadace25f373bdf3d66389632c13
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-09-21 22:03:36 +0100

    added 6.3

---
+++ Chapter_Six/Number_Three.py
@@ -0,0 +1,6 @@
+"""
+The code goes through each day (k) and its list of temperatures (v) in the dictionary.
+For each day, it calculates the average temperature by adding all the temperatures with sum(v),
+dividing by how many there are with len(v),
+then prints the day and its average formatted to two decimal places.
+"""
