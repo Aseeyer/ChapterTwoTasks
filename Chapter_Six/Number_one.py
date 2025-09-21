579ec39f6914bd4c49f2ea825115023eb12cb551
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-09-21 20:48:48 +0100

    Chapter Six

---
+++ Chapter_Six/Number_one.py
@@ -0,0 +1,21 @@
+# Discussion: Dictionary Methods
+
+
+"""
+
+a) In Python, there’s no dedicated add method for dictionaries.
+To add a new key–value pair, you simply assign it directly, for example: my_dict["name"] = "John".
+
+
+b) The keys() method returns a view object of all the keys in the dictionary,
+almost like seeing the dictionary’s headings at a glance.
+
+
+c) The values() method returns a view object containing all the values stored in the dictionary,
+letting you see the data itself without the keys.
+
+
+d) The items() method gives you both keys and values together as (key, value) pairs,
+which is especially useful when you want to loop through the entire dictionary and access both parts at once
+
+"""
