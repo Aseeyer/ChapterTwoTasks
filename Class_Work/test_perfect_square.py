82afc29bdf5c5d338f4e999f3679f13b94d8a48b
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-10-25 20:29:07 +0100

    test for perfect square

---
+++ Class_Work/test_perfect_square.py
@@ -0,0 +1,6 @@
+import unittest
+import perfect_square
+
+class TestForPerfectSquare(unittest.TestCase):
+    def test_that_get_perfect_square_function_exists(self):
+        dollar.get_perfect_square()
